80efa850231606bd916821d8f0aaebb39ca97f03
Elisabelar Marie <melisabe@localhost.localdomain>
2017-10-03 14:02:26 +0200

    genetique

--- creer_deux_edt.m
+++ creer_deux_edt.m
@@ -2,11 +2,32 @@ function [  ] = creer_deux_edt(ind_maman, ind_papa, generation_edt, ind_fils, in
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+chance_mutation = 0.1; %chance qu'un edt mute de maniere aléatoire
+
 for i=1:size(generation_edt,2)
     choix_gene=randi(2);
-    %papa
+    %papa donne au fils, maman à la fille
     if choix_gene=1
         generation_edt(i,ind_fils) = generation_edt(i,ind_papa);
+        generation_edt(i,ind_fille) = generation_edt(i,ind_maman);
+    %maman donne au fils, papa à la fille
+    else
+        generation_edt(i,ind_fils) = generation_edt(i,ind_maman);
+        generation_edt(i,ind_fille) = generation_edt(i,ind_papa);
+    end
+    
+    %mutation
+    i = rand();
+    if i<=chance_mutation
+        %choix du gene à muter
+        gene_mute = randi(size(generation_edt(1)));
+        %modifier cette valeur de l'edt fils (seul le fils mute)
+        if generation_edt(gene_mute,ind_fils) == 0
+            generation_edt(gene_mute,ind_fils) = 1;
+        else
+            generation_edt(i,ind_fils) = 0;
+        end
+    end
 
 end
 
--- genetique.m
+++ genetique.m
@@ -1,9 +1,9 @@
 % Méthode génétique 
 
 %Generation aléatoire de la population d'emplois du temps
-nb_edt = 20;
+nb_edt = 20; % nb edt générés aléatoirement au début de l'algo et traités lors de la recherche de solutions
 premiere_gen = generation_init( nb_edt );
-trouve = false;
+trouve = false; %indique si on a trouvé un edt satisfaisant
 NB_MAX_ITER = 100;
 iter = 1;
 
@@ -18,14 +18,15 @@ while (~trouve &&  iter <= NB_MAX_ITER)
     
     indices_choisis=ind_tri(1:nb_edt/2);
     indices_non_choisis=ind_tri((nb_edt/2)+1:nb_edt);
+    
     %Melanger les indices pour créer des paires aléatoires parmis les
     %meilleurs edt
-    indices_choisis(randperm(length(indices_choisis));
+    indices_choisis(randperm(length(indices_choisis)));
     
     %Créer nb_edt/4 paires d'edt
-    for i=1:2:(nb_edt/4)
-        creer_deux_edt(indices_choisis(i), indices_choisis(i+1), generation_edt, indices_non_choisis(i), indices_non_choisis(i+1));
-        
+    for j=1:2:(nb_edt/4)
+        creer_deux_edt(indices_choisis(j), indices_choisis(j+1), generation_edt, indices_non_choisis(j), indices_non_choisis(j+1));
+    end
     
-    
+    iter++;
 end 
