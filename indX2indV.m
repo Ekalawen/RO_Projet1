b6ddf68ce35555df60fc681c2b1ae28d58d34db7
Pobel Yanis <ypobel@localhost.localdomain>
2017-10-03 11:05:00 +0200

    préconditions ajoutés

--- indX2indV.m
+++ indX2indV.m
@@ -1,7 +1,10 @@
 function [ ind_V ] = indX2indV(i, j, k)
 
-ind_V = 40*(i-1) + 20*(j-1) + k;
-return
-
+if i<=p & j<=c & k<=d*t
+    ind_V = 40*(i-1) + 20*(j-1) + k;
+    return
+else
+    disp("prof (i), promo (j), créneau (k) inexistant dans indX2indV(i,j,k)");
+end
 end
 
